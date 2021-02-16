6bf303a62e9ec5fab598143ab9d25388bcb8ee7e
semircohohvic <semircohovic@gmail.com>
2021-02-16 17:25:36 +0100

    finished app

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tweet;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TweetController extends Controller
+{
+    /*
+    **
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        // check logged user
+        $user=Auth::user();
+        if(!is_null($user)) {
+            $tweets=Tweet::where("user_id", $user->id)->get();
+            if(count($tweets) > 0) {
+                return response()->json(["status" => "success", "count" => count($tweets), "data" => $tweets], 200);
+            }
+
+            else {
+                return response()->json(["status" => "failed", "count" => count($tweets), "message" => "Failed! no tweet found"], 200);
+            }
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+
+        if (!is_null($user)) {
+            $validator = Validator::make($request->all(), [
+                'tweet' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(["status" => "failed", "validation_errors" => $validator->errors()]);
+            }
+            $tweetInput = $request->all();
+            $tweetInput['user_id'] = $user->id;
+
+            $tweet = Tweet::create($tweetInput);
+            if (!is_null($tweet)) {
+                return response()->json(['status' => 'success', 'message' => 'Success! tweet created', 'data' => $tweet]);
+            } else {
+                return response()->json(['status' => 'failed', 'message' => 'Whoops! task not created"']);
+            }
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = Auth::user();
+        if (!is_null($user)) {
+            $tweet = Tweet::where('user_id', $user->id)->where("id", $id)->first();
+            if (!is_null($tweet)) {
+                return response()->json(['status' => "success", 'data' => $tweet], 200);
+            } else {
+                return response()->json(['status' => 'failed', 'message' => 'Failed! no tweet found'], 200);
+            }
+        } else {
+            return response()->json(['status' => 'failed', 'message' => 'Un-authorized user'], 403);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tweet $tweet)
+    {
+        $input = $request->all();
+        $user = Auth::user();
+
+        if (!is_null($user)) {
+            $validator = Validator::make($request->all(), [
+                'tweet' => 'required'
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['status' => 'failed', 'validation_errors' => $validator->errors()]);
+            }
+            $update = $tweet->update($request->all());
+
+            return response()->json(['status' => 'success', 'message' => 'Success! tweet updated', 'data' => $tweet], 200);
+        } else {
+            return response()->json(['status' => 'failed', 'message' => 'Un-authorized user'], 403);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tweet $tweet)
+    {
+        $user = Auth::user();
+        if (!is_null($user)) {
+            $tweet = Tweet::where('id', $tweet)->where('user_id', $user->id)->delete();
+            return response()->json(['status' => 'success', 'message' => 'Success!! task deleted'], 200);
+        } else {
+            return response()->json(["status" => "failed", "message" => "Un-authorized user"], 403);
+        }
+    }
+
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class UserController extends Controller
+{
+   public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+                "name"  =>  "required",
+                "email"  =>  "required|email",
+                "password"  =>  "required"
+        ]);
+        if($validator->fails()) {
+            return response()->json(["status" => "failed", "validation_errors" => $validator->errors()]);
+        }
+
+        $inputs = $request->all();
+        $inputs["password"] = Hash::make($request->password);
+        $user=User::create($inputs);
+        if(!is_null($user)) {
+            return response()->json(["status" => "success", "message" => "Success! registration completed", "data" => $user]);
+        }
+        else {
+            return response()->json(["status" => "failed", "message" => "Registration failed!"]);
+        }
+   }
+   public function login(Request $request) {
+       $validator = Validator::make($request->all(), [
+            "email" =>  "required|email",
+            "password" =>  "required",
+       ]);
+       if($validator->fails()) {
+        return response()->json(["validation_errors" => $validator->errors()]);
+       }
+       $user = User::where("email", $request->email)->first();
+       if(is_null($user)) {
+           return response()->json(["status" => "failed", "message" => "Failed! email not found"]);
+       }
+       if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+           $user       =       Auth::user();
+           $token      =       $user->createToken('token')->plainTextToken;
+
+           return response()->json(["status" => "success", "login" => true, "token" => $token, "data" => $user]);
+       }
+       else {
+           return response()->json(["status" => "failed", "success" => false, "message" => "Whoops! invalid password"]);
+       }
+   }
+   public function user() {
+       $user = Auth::user();
+        if(!is_null($user)){
+            return response()->json(["status" => "success", "data" => $user]);
+        } else {
+            return response()->json(["status" => "failed", "message" => "Whoops! no user found"]);
+        }
+   }
+}
